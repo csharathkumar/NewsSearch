816bc41de1c92f2d8323e5ee7399e708a16d6953
Sharath-Cirupati <csharathkumar@gmail.com>
2016-07-26 00:24:21 -0400

    Added some UI for search interface

---
+++ app/src/main/java/com/codepath/newssearch/util/Constants.java
@@ -0,0 +1,10 @@
+package com.codepath.newssearch.util;
+
+/**
+ * Created by Sharath on 7/26/16.
+ */
+public interface Constants {
+    String API_KEY = "eac0ddf1cc21420ba6661e51b887507c";
+    String BASE_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+}
